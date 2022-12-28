92cf350a3b60ee0aa48fde40ababc0ecfe810e20
sivlad <66171079+sivlad@users.noreply.github.com>
2022-12-28 23:49:46 +0200

    split Controllers to different classes.

---
+++ src/main/java/ua/com/foxminded/UniversityCms/controller/GroupController.java
@@ -0,0 +1,36 @@
+package ua.com.foxminded.UniversityCms.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import ua.com.foxminded.UniversityCms.service.groupservice.GroupService;
+
+@Controller
+public class GroupController {
+
+    @Autowired
+    private GroupService groupService;
+
+    @RequestMapping(value = "/groups/get", method = RequestMethod.GET)
+    public String getGroups(Model model) {
+        model.addAttribute("groups", groupService.findAll());
+        return "groups";
+    }
+
+    @RequestMapping(value = "/groups/update", method = RequestMethod.POST)
+    public String updateGroup(Model model) {
+        return null;
+    }
+
+    @RequestMapping(value = "/groups/create", method = RequestMethod.POST)
+    public String createGroup(Model model) {
+        return null;
+    }
+
+    @RequestMapping(value = "/groups/delete", method = RequestMethod.POST)
+    public String deleteGroup(Model model) {
+        return null;
+    }
+}
--- src/main/java/ua/com/foxminded/UniversityCms/controller/UniversityController.java
+++ src/main/java/ua/com/foxminded/UniversityCms/controller/UniversityController.java
@@ -1,57 +1,16 @@
 package ua.com.foxminded.UniversityCms.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import ua.com.foxminded.UniversityCms.service.groupservice.GroupService;
-import ua.com.foxminded.UniversityCms.service.studentservice.StudentService;
-import ua.com.foxminded.UniversityCms.service.subjectservice.SubjectService;
-import ua.com.foxminded.UniversityCms.service.teacherservice.TeachersService;
 
 @Controller
 public class UniversityController {
 
-    @Autowired
-    private StudentService studentService;
-
-    @Autowired
-    private TeachersService teachersService;
-
-    @Autowired
-    private SubjectService subjectService;
-
-    @Autowired
-    private GroupService groupService;
-
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String indexPage(Model model) {
         return "index";
-    }
-
-    @RequestMapping(value = "/students", method = RequestMethod.GET)
-    public String studentsPage(Model model) {
-        model.addAttribute("students", studentService.findAll());
-        return "students";
-    }
-
-    @RequestMapping(value = "/teachers", method = RequestMethod.GET)
-    public String teachersPage(Model model) {
-        model.addAttribute("teachers", teachersService.findAll());
-        return "teachers";
-    }
-
-    @RequestMapping(value = "/subjects", method = RequestMethod.GET)
-    public String subjectsPage(Model model) {
-        model.addAttribute("subjects", subjectService.findAll());
-        return "subjects";
-    }
-
-    @RequestMapping(value = "/groups", method = RequestMethod.GET)
-    public String groupsPage(Model model) {
-        model.addAttribute("groups", groupService.findAll());
-        return "groups";
     }
 
 }
