dda838d761f4d9c54c5d10e200e1153b66fff7ba
sivlad <66171079+sivlad@users.noreply.github.com>
2023-01-04 00:54:17 +0200

    add authenticationEntryPoint, remove printWriter.

--- src/main/java/ua/com/foxminded/UniversityCms/configuration/MyBasicAuthenticationEntryPoint.java
+++ src/main/java/ua/com/foxminded/UniversityCms/configuration/MyBasicAuthenticationEntryPoint.java
@@ -7,22 +7,21 @@ import org.springframework.stereotype.Component;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 @Component
 public class MyBasicAuthenticationEntryPoint extends BasicAuthenticationEntryPoint {
+
+    private static final String realName = "user";
 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authEx) throws IOException {
         response.addHeader("WWW-Authenticate", "Basic realm=\"" + getRealmName() + "\"");
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-        PrintWriter writer = response.getWriter();
-        writer.println("HTTP Status 401 - " + authEx.getMessage());
     }
 
     @Override
     public void afterPropertiesSet() {
-        setRealmName("Baeldung");
+        setRealmName(realName);
         super.afterPropertiesSet();
     }
 }
