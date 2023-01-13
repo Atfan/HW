d315fdf561473a59c3a16042c108d89fefbd0bd7
sivlad <66171079+sivlad@users.noreply.github.com>
2023-01-13 12:49:20 +0200

    add some tests

---
+++ src/test/java/ua/com/foxminded/UniversityCms/controller/SubjectControllerTest.java
@@ -0,0 +1,12 @@
+package ua.com.foxminded.UniversityCms.controller;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+public class SubjectControllerTest {
+
+    private MockMvc mockMvc;
+
+
+}
